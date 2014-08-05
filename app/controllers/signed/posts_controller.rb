baa213cdfecf398b9e9d3ec849cd5d54487adb64
prathap <prataplachibtech@gmail.com>
2014-08-05 15:57:01 +0530

    upto posts

--- app/controllers/signed/posts_controller.rb
+++ app/controllers/signed/posts_controller.rb
@@ -1,5 +1,18 @@
 class Signed::PostsController < Signed::BaseController
 	def index
-		@posts = Post.all		
+		@posts = Post.all
+		@post = Post.new	
 	end
+	def create
+		@post = current_user.posts.new(post_params)
+
+		if @post.save
+			redirect_to signed_posts_path
+        else
+            render :index
+        end
+    end
+    def post_params
+        params.require(:post).permit(:content)
+    end    
 end
